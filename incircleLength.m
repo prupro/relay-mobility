ef235f22fa24372ac899f893a1aa0434c3fef46e
prupro <pruviola@gmail.com>
2016-05-28 11:39:41 +0530

    pass center as well and check inside the function

--- incircleLength.m
+++ incircleLength.m
@@ -1,12 +1,17 @@
-function r0 = incircleLength(i1,i2,radius)
+function r0 = incircleLength(i1,i2,radius,center)
 %% 
 % given initial position i1 and final position i2
 % i1 = [1,0];
 % i2 = [-1,2];
+    i1 = i1-center;
+    i2 = i2-center;
+    if norm(i2) < radius
+        r0 = norm(i2-i1);
+    else
+        r2 = norm(i1);
+        r1 = radius; %radius
+        theta=acos(dot(i2-i1,i1)/(r2*norm(i2-i1)));
 
-    r2 = norm(i1);
-    r1 = radius; %radius
-    theta=acos(dot(i2-i1,i1)/(r2*norm(i2-i1)));
-
-    r0 = -r2*cos(theta) + sqrt( r1^2 - (r2*sin(theta))^2);
+        r0 = -r2*cos(theta) + sqrt( r1^2 - (r2*sin(theta))^2);
+    end
 end
