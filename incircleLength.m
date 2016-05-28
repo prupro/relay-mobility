50dcea089cd1da7483f9f4b4025becc543e3fa95
prupro <pruviola@gmail.com>
2016-05-28 11:06:19 +0530

    pass radius as a parameter

--- incircleLength.m
+++ incircleLength.m
@@ -1,11 +1,11 @@
-function r0 = incircleLength(i1,i2)
+function r0 = incircleLength(i1,i2,radius)
 %% 
 % given initial position i1 and final position i2
 % i1 = [1,0];
 % i2 = [-1,2];
 
     r2 = norm(i1);
-    r1 = 1; %radius
+    r1 = radius; %radius
     theta=acos(dot(i2-i1,i1)/(r2*norm(i2-i1)));
 
     r0 = -r2*cos(theta) + sqrt( r1^2 - (r2*sin(theta))^2);
