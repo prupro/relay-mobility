01939ffcff2d9a22ec06c19b0c23019ffaed0d7f
prupro <pruviola@gmail.com>
2016-05-28 10:57:43 +0530

    function to calculate length inside the circle

---
+++ incircleLength.m
@@ -0,0 +1,12 @@
+function r0 = incircleLength(i1,i2)
+%% 
+% given initial position i1 and final position i2
+% i1 = [1,0];
+% i2 = [-1,2];
+
+    r2 = norm(i1);
+    r1 = 1; %radius
+    theta=acos(dot(i2-i1,i1)/(r2*norm(i2-i1)));
+
+    r0 = -r2*cos(theta) + sqrt( r1^2 - (r2*sin(theta))^2);
+end
