890668dbb012fe8bbaaa072fff4439016f48e9e4
prupro <pruviola@gmail.com>
2016-05-31 18:17:28 +0530

    returns radius if initial position is origin

--- incircleLength.m
+++ incircleLength.m
@@ -9,9 +9,13 @@ function r0 = incircleLength(i1,i2,radius,center)
         r0 = norm(i2-i1);
     else
         r2 = norm(i1);
+        if r2 == 0
+            r0 = radius;
+        else
         r1 = radius; %radius
         theta=acos(dot(i2-i1,i1)/(r2*norm(i2-i1)));
 
         r0 = -r2*cos(theta) + sqrt( r1^2 - (r2*sin(theta))^2);
+        end
     end
 end
