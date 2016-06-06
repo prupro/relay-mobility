d01c09d792551d3951aa6637eda49a3c51714989
prupro <pruviola@gmail.com>
2016-06-06 13:38:42 +0530

    verify for some r,theta

--- outProb.m
+++ outProb.m
@@ -1,22 +1,26 @@
-R1 = 10;
-R2 = R1;
+R1 = 150;
+R2 = 0.4*R1;
+r = (R1-R2)/2;
+theta = 0; %acos( 1 - R2*R2/(2*R1*R1) );
 lambda = 0.01;
 
-beta1 = @(theta) acos( 1 - R2*R2/(2*R1*R1) ) - theta ;
+
+beta1 = acos( 1 - R2*R2/(2*R1*R1) ) - theta;
 beta2 = acos( 1 - R2*R2/(2*R1*R1) );
 
-alpha1 = @(r,theta) theta + atan( R1*sin(beta1(theta))/(R1*cos(beta1(theta)) - r) );
-alpha2 = @(r,theta) atan( (r*sin(theta) + R1*sin(beta2))/(R1*cos(beta2) - r*cos(theta)));
 
-r11 = @(r,theta,alpha) -r*cos(theta-alpha) + sqrt( R1*R1 - r*r * sin(theta-alpha)*sin(theta-alpha) );
-r12 = @(r,theta,alpha) R1*cos(alpha) - r*cos(alpha-theta) + sqrt( (R1*cos(alpha)-r*cos(alpha-theta))*(R1*cos(alpha)-r*cos(alpha-theta)) - r*r*sin(theta)*sin(theta) - (R1-r*cos(theta))^2 + R2*R2 );
+alpha1 = theta + atan( R1*sin(beta1)/(R1*cos(beta1) - r) );
+alpha2 = atan( (r*sin(theta) + R1*sin(beta2))/(R1*cos(beta2) - r*cos(theta)));
 
-a1 = alpha1(r,theta);
-a2 = alpha2(r,theta);
+r11 = @(alpha) -r*cos(theta-alpha) + sqrt( R1*R1 - r*r * sin(theta-alpha).*sin(theta-alpha) );
+r12 = @(alpha) R1*cos(alpha) - r*cos(alpha-theta) + sqrt( (R1*cos(alpha)-r*cos(alpha-theta)).*(R1*cos(alpha)-r*cos(alpha-theta)) - r*r*sin(theta)*sin(theta) - (R1-r*cos(theta))^2 + R2*R2 );
+
+a1 = alpha1;
+a2 = alpha2;
 if a1 < 0
     a1 = pi + a1;
     a2 = pi + a2;
 end
-f1 = @(alpha) (1/2*pi)*exp(-2*pi*lambda*r11(r,theta,alpha)*r11(r,theta,alpha));
-f2 = @(alpha) (1/2*pi)*exp(-2*pi*lambda*r12(r,theta,alpha)*r12(r,theta,alpha));
-Prob = integral(@(alpha)f1,-a2,a1) +  integral(@(alpha)f2,2*pi-a2);
+f1 = @(alpha) (1/2/pi)*exp(-2*pi*lambda*r11(alpha).*r11(alpha));
+f2 = @(alpha) (1/2/pi)*exp(-2*pi*lambda*r12(alpha).*r12(alpha));
+Prob = integral(f1,-a2,a1) +  integral(f2,a1,2*pi-a2)
