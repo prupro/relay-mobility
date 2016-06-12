fb3f32e6aef38ae41a95a06367021e49a7d0f2c9
prupro <pruviola@gmail.com>
2016-06-12 19:07:16 +0530

    collect results. first up:rwpStat

--- rwpStat.m
+++ rwpStat.m
@@ -1,12 +1,17 @@
 origin = [0,0];
-L = 100; 
+L = 1000; 
 area = L*L;
-lambda = 0.01; % rwp density
+lambdaS = 0.001:0.0099:0.1; % rwp density
 
+meanLegLengthSim = zeros(1,numel(lambdaS));
+meanLegLength = zeros(1,numel(lambdaS));
 noOfLegs = 100;
 
 noOfPaths = 10;
 totalDist = zeros(1,noOfPaths);
+
+for k = 1:numel(lambdaS)
+    lambda = lambdaS(k);
 
 for j = 1:noOfPaths
 
@@ -33,4 +38,12 @@ end
 
 totalDist(j) = sum(distTravelled);
 end
-sum(totalDist)/(noOfLegs*noOfPaths)
+meanLegLengthSim(k) = sum(totalDist)/(noOfLegs*noOfPaths);
+meanLegLength(k) = 1/(2*sqrt(lambda));
+end
+
+plot(lambdaS,meanLegLength,'r',lambdaS,meanLegLengthSim);
+legend('analytic','simulation');
+title('Mean Transition Length');
+xlabel('\lambda');
+ylabel('E[L]');
